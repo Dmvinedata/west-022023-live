e4e47f41753869f8da1b888be7c4e33d3bb02841
rothberry <phil.roth077@gmail.com>
2023-03-01 14:17:11 -0800

    patch delte done!

--- 1-phase/08-patch-delete/src/index.js
+++ 1-phase/08-patch-delete/src/index.js
@@ -3,12 +3,23 @@
 
     Review Questions!
 
-    1. What is the difference between PUT and PATCH?
+    1. What is the difference between PUT and PATCH?\
+			ex in the db{
+				id: 1, name: "james", job: "dev"
+			}
+			request body: {name: "derrick"}
+			- PUT? 			Updates ALL the elements
+			- PATCH? 		Update only the requested elements
+		
+		2. What is the route/url/path for patch and deletes?
+			index => /books
+			patch => /books/:id
+			delete => /books/:id
 
 */
 
 // ! GLOBAL VARIABLES
-const BASE_URL = "http://localhost:3000/books"
+const BASE_URL = "http://localhost:3000/books/"
 
 // ! IMPORTANT GLOBAL DOM ELEMENTS
 const bookList = document.getElementById("book-list")
@@ -29,12 +40,19 @@ const clearList = () => {
 	bookList.innerHTML = ""
 }
 
-const renderOneBook = (bookObj) => {
+const renderOneBook = (book) => {
 	const li = document.createElement("li")
 	const h3 = document.createElement("h3")
 	const pAuthor = document.createElement("p")
 	const pPrice = document.createElement("p")
 	const img = document.createElement("img")
+	// create an INPUT field
+	const inventoryInput = document.createElement("input")
+	inventoryInput.type = "number"
+	// inventoryInput.placeholder = "Please Enter your input"
+	inventoryInput.value = book.inventory
+	const pInventory = document.createElement("p")
+	pInventory.textContent = book.inventory
 
 	h3.textContent = book.title
 	pAuthor.textContent = `Author: ${book.author}`
@@ -42,6 +60,28 @@ const renderOneBook = (bookObj) => {
 
 	img.src = book.imageUrl
 	li.className = "list-li"
+	li.id = `book-${book.id}`
+
+	const deleteBtn = document.createElement("button")
+	deleteBtn.textContent = "DELETE"
+
+	inventoryInput.addEventListener("change", (e) => {
+		// updateBook(newInvValue, bookID)
+		// pInventory.textContent = e.target.value
+		updateBook(e.target.value, book.id, pInventory)
+	})
+
+	deleteBtn.addEventListener("click", (e) => {
+		// * OPTIMISTIC REMOVAL
+		// li.remove()
+		// ! For PESSIMISTIC, we need to pass in something that can point us to the liContainer
+		// can be the EVENT, LI, etc...
+		// deleteBook(book.id, li)
+		deleteBook(book.id, e)
+	})
+
+	li.append(deleteBtn, h3, pAuthor, pPrice, pInventory, inventoryInput, img)
+	bookList.append(li)
 }
 
 const handleForm = (event) => {
@@ -81,6 +121,57 @@ const createBook = (bookObjToAdd) => {
 			renderOneBook(newBook)
 		})
 		.catch((err) => console.log(err))
+}
+
+// ! NEW STUFFF
+
+const updateBook = (newInventoryValue, bookID, inventoryElement) => {
+	console.log(newInventoryValue, bookID)
+
+	const patchReqObj = {
+		method: "PATCH",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify({
+			inventory: newInventoryValue,
+		}),
+	}
+
+	fetch(BASE_URL + bookID, patchReqObj)
+		.then((res) => res.json())
+		.then((updatedBookObj) => {
+			// console.log(updatedBookObj)
+			// console.log(inventoryElement)
+			inventoryElement.textContent = updatedBookObj.inventory
+		})
+}
+
+const deleteBook = (bookID) => {
+	console.log("DELETING!", bookID)
+	// debugger
+	fetch(BASE_URL + bookID, {
+		method: "DELETE",
+	})
+		// we don't always need to parse to json because the delete response from the json-server is always an empty object
+		// // .then((res) => res.json())
+		.then((uselessLocalVariable) => {
+			// remove the li node from the list
+			// event.target.parentElement.remove()
+			// liContainer.remove()
+			document.getElementById(`book-${bookID}`).remove()
+			// console.log(bookID, liContainer)
+			// debugger
+		})
+		.catch((err) => console.log(err))
+
+	// ! the reponses for a delete request are up to the dev
+	// HEAD NO CONTENT
+	// CANNOT USE res.json() because there will be no response
+	// Is empty object/array/datatype (json-server)
+	// Send the object that was deleted from the database
+	// if YOU have more control
 }
 
 const init = () => {
