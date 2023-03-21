5a23ced04d3f191ed992875c181e53412c74fe35
rothberry <phil.roth077@gmail.com>
2023-03-20 17:21:00 -0700

    useEffect and POST done!

--- 2-phase/05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 2-phase/05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,13 +1,96 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Header from "./components/Header"
 import ProjectForm from "./components/ProjectForm"
 import ProjectList from "./components/ProjectList"
+
+/* 
+
+  Warm Up Questions!
+
+  1. What is RESTful Routing?
+    - Convention for organizing urls, routes, 
+      - INDEX   GET  /somethings
+      - SHOW    GET ONE THING /somethings/:id
+      - CREATE  POST      /somethings
+      - UPDATE  PATCH     /somethings/:id
+      - DELETE  DELETE    /somethings/:id
+
+  2. How do we lift state?
+    - Move state to the parent and pass down as props
+
+  3. What is a Controlled Form?
+    - Every input on the form is in State 
+    - AND the value of the inputs is taken PESSIMISTICALLY from our state
+      - as simple as adding the value={valueFromState} on the <input />
+    - Helps with making sure that the state is 100% accurate 
+
+*/
+
+/* 
+
+  ! The Component LifeCycle
+
+  - What happens before, during, and after the comp renders
+
+  3 phases of a comp lifecycle
+    1. Mounting
+      - Everything that happens when the comp is put on the page
+    2. Updating
+      - while the comp is rendered, any changes to the comp
+    3. Unmounting
+      - What happens once the comp unrenders
+      - Cleanup
+
+    USEEFFECT HOOK
+
+*/
 
 const App = () => {
 	const [projects, setProjects] = useState([])
 	const [isDarkMode, setIsDarkMode] = useState(true)
 
-	const handleClick = () => {
+	// useEffect(cbFunc, dependecies)
+	// * Without a dependency array, this useEffect will run whenever there is ANY change to state on this component
+	useEffect(() => {
+		// This is where we define what this effect does
+		console.log("hi, I will trigger on EVERY SINGLE STATE CHANGE")
+		// ! DO NOT PUT CONTINUOUS LOOPS, FETCH REQUESTS
+		// * DO PUT DEBUGGING CONSOLELOGS
+	})
+
+	// ! DON'T have this AMIBOUS Console.log, instead put it in an  useEffect Hook
+	// // console.log(projects)
+
+	// * EMPTY DEP ARRAY
+	// ! COMPONENTDIDMOUNT
+	// Check the dependecy array to trigger the hook
+	// By the dep array being empty, that means that it will only run once on the mount
+	useEffect(() => {
+		console.log("Component Did Mount...")
+		// * For initial fetches
+		// * For any outside additional data
+		fetchAllProjects()
+
+		return function cleanup() {
+			// ! Will only run is the entire APP unmounts
+			console.log("ATTEMPT TO UNMOUNT")
+		}
+	}, [])
+
+	useEffect(() => {
+		// Only run when the state of the darkMode changes
+		console.log({ isDarkMode })
+		return () => {
+			console.log("CLEANING UP DARKMODE")
+		}
+	}, [isDarkMode])
+
+	// ! DO NOT CHANGE SET STATE INSIDE OF UNCONTROLLED USEEFFECT
+	// useEffect(() => {
+	// 	setIsDarkMode(!isDarkMode)
+	// })
+
+	const fetchAllProjects = () => {
 		fetch("http://localhost:4000/projects")
 			.then((res) => res.json())
 			.then((projects) => setProjects(projects))
@@ -15,11 +98,12 @@ const App = () => {
 
 	const onToggleDarkMode = () => setIsDarkMode(!isDarkMode)
 
+	// * RENDERING FUNCTION
 	return (
 		<div className={isDarkMode ? "App" : "App light"}>
 			<Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-			<ProjectForm />
-			<button onClick={handleClick}>Load Projects</button>
+			<ProjectForm setProjects={setProjects} projects={projects} />
+			{/* <button onClick={handleClick}>Load Projects</button> */}
 			<ProjectList projects={projects} />
 		</div>
 	)
--- 2-phase/05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
+++ 2-phase/05_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
@@ -1,37 +1,108 @@
-import React from "react";
+import { useState } from "react"
 
-const ProjectForm = () => {
-  return (
-    <section>
-      <form className="form" autoComplete="off">
-        <h3>Add New Project</h3>
+const ProjectForm = ({ setProjects, projects }) => {
+	const [formState, setState] = useState({
+		name: "",
+		about: "",
+		phase: "0",
+		link: "",
+		image: "",
+		// image_url: "",
+	})
+	const { name, about, phase, link, image } = formState
 
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+	const handleChange = (e) => {
+		setState({ ...formState, [e.target.name]: e.target.value })
+	}
 
-        <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(formState)
+		// ! POST REQUEST
 
-        <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
-          <option>Select One</option>
-          <option value="1">Phase 1</option>
-          <option value="2">Phase 2</option>
-          <option value="3">Phase 3</option>
-          <option value="4">Phase 4</option>
-          <option value="5">Phase 5</option>
-        </select>
+		const postReqobj = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ...formState, phase: Number(phase) }),
+			// body: JSON.stringify(formState),
+		}
+		console.log(postReqobj)
 
-        <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+		// ! Optimism...
+		// setProjects((prevStateProjects) => [...prevStateProjects, formState])
 
-        <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+		fetch("http://localhost:4000/projects", postReqobj)
+			.then((res) => res.json())
+			.then((newProject) => {
+				console.log(newProject)
+				// Set the state of the projects here!
+				// How do we PUSH into state
+				// setProjects([...projects, newProject])
+				setProjects((prevStateProjects) => [...prevStateProjects, newProject])
+			})
+	}
 
-        <button type="submit">Add Project</button>
-      </form>
-    </section>
-  );
-};
+	return (
+		<section>
+			<form className="form" autoComplete="off" onSubmit={handleSubmit}>
+				<h3>Add New Project</h3>
 
-export default ProjectForm;
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					onChange={handleChange}
+					value={name}
+				/>
+
+				<label htmlFor="about">About</label>
+				<textarea
+					id="about"
+					name="about"
+					onChange={handleChange}
+					value={about}
+				/>
+
+				<label htmlFor="phase">Phase</label>
+				<select
+					name="phase"
+					id="phase"
+					onChange={handleChange}
+					value={formState.phase}
+				>
+					<option value="0">Select One</option>
+					<option value="1">Phase 1</option>
+					<option value="2">Phase 2</option>
+					<option value="3">Phase 3</option>
+					<option value="4">Phase 4</option>
+					<option value="5">Phase 5</option>
+				</select>
+
+				<label htmlFor="link">Project Homepage</label>
+				<input
+					type="text"
+					id="link"
+					name="link"
+					onChange={handleChange}
+					value={link}
+				/>
+
+				<label htmlFor="image">Screenshot</label>
+				<input
+					type="text"
+					id="image"
+					name="image"
+					onChange={handleChange}
+					value={image}
+				/>
+
+				<button type="submit">Add Project</button>
+			</form>
+		</section>
+	)
+}
+
+export default ProjectForm
