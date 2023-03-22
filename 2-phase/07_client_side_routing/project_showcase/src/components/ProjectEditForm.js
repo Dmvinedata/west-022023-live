c24e484ac96617e9196d82c09d5cbee2299b705a
rothberry <phil.roth077@gmail.com>
2023-03-22 11:00:28 -0700

    client routing starterv2

--- 2-phase/07_client_side_routing/project_showcase/src/components/ProjectEditForm.js
+++ 2-phase/07_client_side_routing/project_showcase/src/components/ProjectEditForm.js
@@ -1,95 +1,91 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 
 const ProjectEditForm = ({ onUpdateProject }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    about: "",
-    phase: "",
-    link: "",
-    image: "",
-  });
+	const [formData, setFormData] = useState({
+		name: "",
+		about: "",
+		phase: "",
+		link: "",
+		image: "",
+	})
 
-  const { name, about, phase, link, image } = formData;
+	const { name, about, phase, link, image } = formData
 
-  // const { id } = useParams()
+	useEffect(() => {
+		fetch(`http://localhost:4000/projects/1`)
+			.then((res) => res.json())
+			.then((project) => setFormData(project))
+	}, [])
 
-  // const history = useHistory()
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		setFormData({ ...formData, [name]: value })
+	}
 
-  useEffect(() => {
-    fetch(`http://localhost:4000/projects/1`)
-      .then((res) => res.json())
-      .then((project) => setFormData(project));
-  }, []);
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const configObj = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			body: JSON.stringify(formData),
+		}
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+		fetch(`http://localhost:4000/projects/1`, configObj)
+			.then((resp) => resp.json())
+			.then((updatedProj) => {
+				onUpdateProject(updatedProj)
+			})
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const configObj = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(formData),
-    };
+	return (
+		<form onSubmit={handleSubmit} className="form" autoComplete="off">
+			<h3>Edit Project</h3>
 
-    fetch(`http://localhost:4000/projects/1`, configObj)
-      .then((resp) => resp.json())
-      .then((updatedProj) => {
-        onUpdateProject(updatedProj);
-      });
-  };
+			<label htmlFor="name">Name</label>
+			<input
+				type="text"
+				id="name"
+				name="name"
+				value={name}
+				onChange={handleChange}
+			/>
 
-  return (
-    <form onSubmit={handleSubmit} className="form" autoComplete="off">
-      <h3>Edit Project</h3>
+			<label htmlFor="about">About</label>
+			<textarea id="about" name="about" value={about} onChange={handleChange} />
 
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={name}
-        onChange={handleChange}
-      />
+			<label htmlFor="phase">Phase</label>
+			<select name="phase" id="phase" value={phase} onChange={handleChange}>
+				<option value="1">Phase 1</option>
+				<option value="2">Phase 2</option>
+				<option value="3">Phase 3</option>
+				<option value="4">Phase 4</option>
+				<option value="5">Phase 5</option>
+			</select>
 
-      <label htmlFor="about">About</label>
-      <textarea id="about" name="about" value={about} onChange={handleChange} />
+			<label htmlFor="link">Project Homepage</label>
+			<input
+				type="text"
+				id="link"
+				name="link"
+				value={link}
+				onChange={handleChange}
+			/>
 
-      <label htmlFor="phase">Phase</label>
-      <select name="phase" id="phase" value={phase} onChange={handleChange}>
-        <option value="1">Phase 1</option>
-        <option value="2">Phase 2</option>
-        <option value="3">Phase 3</option>
-        <option value="4">Phase 4</option>
-        <option value="5">Phase 5</option>
-      </select>
+			<label htmlFor="image">Screenshot</label>
+			<input
+				type="text"
+				id="image"
+				name="image"
+				value={image}
+				onChange={handleChange}
+			/>
 
-      <label htmlFor="link">Project Homepage</label>
-      <input
-        type="text"
-        id="link"
-        name="link"
-        value={link}
-        onChange={handleChange}
-      />
+			<button type="submit">Update Project</button>
+		</form>
+	)
+}
 
-      <label htmlFor="image">Screenshot</label>
-      <input
-        type="text"
-        id="image"
-        name="image"
-        value={image}
-        onChange={handleChange}
-      />
-
-      <button type="submit">Update Project</button>
-    </form>
-  );
-};
-
-export default ProjectEditForm;
+export default ProjectEditForm
