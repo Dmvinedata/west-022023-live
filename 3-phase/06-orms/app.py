f33a53e75e37e8217da202cf7f8e7954fffdbff5
rothberry <phil.roth077@gmail.com>
2023-04-10 13:30:18 -0700

    ORMs Starter

---
+++ 3-phase/06-orms/app.py
@@ -0,0 +1,14 @@
+# Our Run File
+import ipdb
+from lib.pet import Pet
+# from lib.owner import Owner
+
+# First need to drop tables/database
+
+
+# Create the Pet table with the class method
+# Create a new instance of Pet 
+# Save new Pet instance to DB
+
+ipdb.set_trace()
+print("DONE!")
